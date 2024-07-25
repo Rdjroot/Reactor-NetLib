b0d30e3077e65c350a568f75558d338ec674966b
rdjroot <rdj_genhub@foxmail.com>
2024-07-25 11:08:04 +0800

    resturct project

--- 15/build/CMakeFiles/clean_logs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_logs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_logs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
